4d75fa32acab6d4fa82aa20d973aea4971bee2e9
jaehyeon48 <imjaehyeon48@gmail.com>
2021-01-20 08:27:20 +0900

    complete restructuring portfolio list to tabular layout

--- src/components/portfolio/PortfolioItem.js
+++ src/components/portfolio/PortfolioItem.js
@@ -105,121 +105,139 @@ const PortfolioItem = ({
   }
 
   return (
-    <React.Fragment>
-      <li className="portfolio-item">
-        <div className="portfolio-item-name">
-          {portfolio.portfolioName}
-        </div>
-        <div className={`portfolio-item-privacy
-          ${portfolio.portfolioPrivacy === 'public' ? "portfolio--public" : "portfolio--private"}`}>
-          {portfolio.portfolioPrivacy}
-        </div>
-        <div className="portfolio-item-default">
-          <span
-            className={`portfolio-default-notice ${isDefault ? "pf--default" : "pf--not-default"}`}
-            onClick={handleChooseDefault}
-          >
-            DEFAULT
+    <tr className="portfolio-item">
+      <td className="portfolio-item-name">
+        {portfolio.portfolioName}
+      </td>
+      <td className="portfolio-item-privacy">
+        {portfolio.portfolioPrivacy === 'public' ? (
+          <span>
+            Public
+            <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="lock-open" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className="portfolio-privacy-icon">
+              <path fill="currentColor" d="M432.3 0C352.8-.2 288 64.5 288 144v48H48c-26.5 0-48 21.5-48 48v224c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V240c0-26.5-21.5-48-48-48h-64v-46.8c0-52.8 42.1-96.7 95-97.2 53.4-.6 97 42.7 97 96v56c0 13.3 10.7 24 24 24s24-10.7 24-24v-54.6C576 65.8 512 .2 432.3 0zM400 240v224H48V240h352z" />
+            </svg>
           </span>
-        </div>
-        <div className="portfolio-item-actions">
-          <Button
-            btnType={'button'}
-            btnText={'Edit'}
-            btnColor={'warning'}
-            onClickFunc={openPfEditModal}
-          />
-          <Button
-            btnType={'button'}
-            btnText={'Delete'}
-            btnColor={'danger'}
-            onClickFunc={openDeleteConfirmModal}
-          />
-          <Button
-            btnType={'button'}
-            btnText={'Detail'}
-            btnColor={'lightGray'}
-          />
-        </div>
-        {isEditModalOpen && (
-          <Modal closeModalFunc={closeEditModal}>
-            <div className="portfolio-form">
-              <input
-                type="text"
-                value={pfNameToBeEdited}
-                onChange={handleEditPfName}
-                data-is-empty={pfNameToBeEdited.length === 0}
-                className={isPfNameEmpty ? "portfolio-form-field portfolio-form-field-error"
-                  : "portfolio-form-field"}
-              />
-              <label
-                className={isPfNameEmpty ? "portfolio-form-label portfolio-form-label-error"
-                  : "portfolio-form-label"}
-              >Edit Portfolio Name</label>
-              <div className="portfolio-privacy-buttons">
-                <button
-                  type="button"
-                  className={`${privacy === 'public' ? 'pf-privacy-btn pf-privacy-checked' : 'pf-privacy-btn'}`}
-                  onClick={privacyToPublic}
-                >
-                  <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="globe" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" >
-                    <path fill="currentColor" d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm193.2 152h-82.5c-9-44.4-24.1-82.2-43.2-109.1 55 18.2 100.2 57.9 125.7 109.1zM336 256c0 22.9-1.6 44.2-4.3 64H164.3c-2.7-19.8-4.3-41.1-4.3-64s1.6-44.2 4.3-64h167.4c2.7 19.8 4.3 41.1 4.3 64zM248 40c26.9 0 61.4 44.1 78.1 120H169.9C186.6 84.1 221.1 40 248 40zm-67.5 10.9c-19 26.8-34.2 64.6-43.2 109.1H54.8c25.5-51.2 70.7-90.9 125.7-109.1zM32 256c0-22.3 3.4-43.8 9.7-64h90.5c-2.6 20.5-4.2 41.8-4.2 64s1.5 43.5 4.2 64H41.7c-6.3-20.2-9.7-41.7-9.7-64zm22.8 96h82.5c9 44.4 24.1 82.2 43.2 109.1-55-18.2-100.2-57.9-125.7-109.1zM248 472c-26.9 0-61.4-44.1-78.1-120h156.2c-16.7 75.9-51.2 120-78.1 120zm67.5-10.9c19-26.8 34.2-64.6 43.2-109.1h82.5c-25.5 51.2-70.7 90.9-125.7 109.1zM363.8 320c2.6-20.5 4.2-41.8 4.2-64s-1.5-43.5-4.2-64h90.5c6.3 20.2 9.7 41.7 9.7 64s-3.4 43.8-9.7 64h-90.5z" />
-                  </svg>
+        ) : (
+            <span>
+              Private
+              <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="lock" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="portfolio-privacy-icon">
+                <path fill="currentColor" d="M400 192h-32v-46.6C368 65.8 304 .2 224.4 0 144.8-.2 80 64.5 80 144v48H48c-26.5 0-48 21.5-48 48v224c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V240c0-26.5-21.5-48-48-48zm-272-48c0-52.9 43.1-96 96-96s96 43.1 96 96v48H128v-48zm272 320H48V240h352v224z" />
+              </svg>
+            </span>
+          )}
+      </td>
+      <td className="portfolio-item-default">
+        {isDefault ? (
+          <span className="portfolio-item__default-text">Default</span>
+        ) : (
+            <Button
+              btnType={'button'}
+              btnText={'Set Default'}
+              onClickFunc={handleChooseDefault}
+            />
+          )}
+      </td>
+      <td className="portfolio-item-edit">
+        <Button
+          btnType={'button'}
+          btnText={'Edit'}
+          btnColor={'warning'}
+          onClickFunc={openPfEditModal}
+        />
+      </td>
+      <td className="portfolio-item-delete">
+        <Button
+          btnType={'button'}
+          btnText={'Delete'}
+          btnColor={'danger'}
+          onClickFunc={openDeleteConfirmModal}
+        />
+      </td>
+      <td className="portfolio-item-detail">
+        <Button
+          btnType={'button'}
+          btnText={'Detail'}
+          btnColor={'blue'}
+        />
+      </td>
+      {isEditModalOpen && (
+        <Modal closeModalFunc={closeEditModal}>
+          <div className="portfolio-form">
+            <input
+              type="text"
+              value={pfNameToBeEdited}
+              onChange={handleEditPfName}
+              data-is-empty={pfNameToBeEdited.length === 0}
+              className={isPfNameEmpty ? "portfolio-form-field portfolio-form-field-error"
+                : "portfolio-form-field"}
+            />
+            <label
+              className={isPfNameEmpty ? "portfolio-form-label portfolio-form-label-error"
+                : "portfolio-form-label"}
+            >Edit Portfolio Name</label>
+            <div className="portfolio-privacy-buttons">
+              <button
+                type="button"
+                className={`${privacy === 'public' ? 'pf-privacy-btn pf-privacy-checked' : 'pf-privacy-btn'}`}
+                onClick={privacyToPublic}
+              >
+                <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="lock-open" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                  <path fill="currentColor" d="M432.3 0C352.8-.2 288 64.5 288 144v48H48c-26.5 0-48 21.5-48 48v224c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V240c0-26.5-21.5-48-48-48h-64v-46.8c0-52.8 42.1-96.7 95-97.2 53.4-.6 97 42.7 97 96v56c0 13.3 10.7 24 24 24s24-10.7 24-24v-54.6C576 65.8 512 .2 432.3 0zM400 240v224H48V240h352z" />
+                </svg>
                   Public
                   </button>
-                <button
-                  type="button"
-                  className={`${privacy === 'private' ? 'pf-privacy-btn pf-privacy-checked' : 'pf-privacy-btn'}`}
-                  onClick={privacyToPrivate}
-                >
-                  <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="lock-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" >
-                    <path fill="currentColor" d="M224 412c-15.5 0-28-12.5-28-28v-64c0-15.5 12.5-28 28-28s28 12.5 28 28v64c0 15.5-12.5 28-28 28zm224-172v224c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V240c0-26.5 21.5-48 48-48h32v-48C80 64.5 144.8-.2 224.4 0 304 .2 368 65.8 368 145.4V192h32c26.5 0 48 21.5 48 48zm-320-48h192v-48c0-52.9-43.1-96-96-96s-96 43.1-96 96v48zm272 48H48v224h352V240z" />
-                  </svg>
+              <button
+                type="button"
+                className={`${privacy === 'private' ? 'pf-privacy-btn pf-privacy-checked' : 'pf-privacy-btn'}`}
+                onClick={privacyToPrivate}
+              >
+                <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="lock" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                  <path fill="currentColor" d="M400 192h-32v-46.6C368 65.8 304 .2 224.4 0 144.8-.2 80 64.5 80 144v48H48c-26.5 0-48 21.5-48 48v224c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V240c0-26.5-21.5-48-48-48zm-272-48c0-52.9 43.1-96 96-96s96 43.1 96 96v48H128v-48zm272 320H48V240h352v224z" />
+                </svg>
                   Private
                   </button>
-              </div>
-              {isPfNameEmpty ? (
-                <p className="notice-pf-name-error">Name Is Empty.</p>
-              ) : null}
-              <Button
-                btnType={'button'}
-                btnText={'EDIT'}
-                btnColor={'warning'}
-                onClickFunc={handleEditPortfolio}
-                isDisabled={isPfNameEmpty}
-              />
             </div>
-          </Modal>
-        )}
-        {isConfirmModalOpen && (
-          <Modal closeModalFunc={closeDeleteConfirmModal}>
-            <div className="delete-confirm">
-              <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="exclamation-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                <path fill="currentColor" d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zm-248 50c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z" />
-              </svg>
-              <p>
-                Do you really want to delete
+            {isPfNameEmpty ? (
+              <p className="notice-pf-name-error">Name Is Empty.</p>
+            ) : null}
+            <Button
+              btnType={'button'}
+              btnText={'EDIT'}
+              btnColor={'warning'}
+              onClickFunc={handleEditPortfolio}
+              isDisabled={isPfNameEmpty}
+            />
+          </div>
+        </Modal>
+      )}
+      {isConfirmModalOpen && (
+        <Modal closeModalFunc={closeDeleteConfirmModal}>
+          <div className="delete-confirm">
+            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="exclamation-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path fill="currentColor" d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zm-248 50c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z" />
+            </svg>
+            <p>
+              Do you really want to delete
                 <span className="to-delete-name">{portfolio.portfolioName}</span>?
               </p>
-              <div className="delete-confirm-actions">
-                <Button
-                  btnType={'button'}
-                  btnText={'Delete'}
-                  btnColor={'danger'}
-                  onClickFunc={handleDeletePortfolio}
-                />
-                <Button
-                  btnType={'button'}
-                  btnText={'Cancel'}
-                  btnColor={'lightGray'}
-                  onClickFunc={closeDeleteConfirmModal}
-                />
-              </div>
+            <div className="delete-confirm-actions">
+              <Button
+                btnType={'button'}
+                btnText={'Delete'}
+                btnColor={'danger'}
+                onClickFunc={handleDeletePortfolio}
+              />
+              <Button
+                btnType={'button'}
+                btnText={'Cancel'}
+                btnColor={'lightGray'}
+                onClickFunc={closeDeleteConfirmModal}
+              />
             </div>
-          </Modal>
-        )}
-      </li>
-    </React.Fragment>
+          </div>
+        </Modal>
+      )}
+    </tr>
   );
 }
 
--- src/components/portfolio/Portfolios.js
+++ src/components/portfolio/Portfolios.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 import PortfolioItem from './PortfolioItem';
 import Button from '../button/Button';
@@ -31,6 +32,7 @@ const Portfolios = ({
   const [isNameEmptyErr, setIsNameEmptyErr] = useState(false);
   const [isPfNameEmpty, setIsPfNameEmpty] = useState(false);
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [currentPortfolioName, setCurrentPortfolioName] = useState('');
 
   useEffect(() => {
     if (isNameEmptyErr && newPortfolioName.trim() !== '') {
@@ -40,6 +42,19 @@ const Portfolios = ({
       setIsPfNameEmpty(true);
     }
   }, [newPortfolioName, isNameEmptyErr]);
+
+  useEffect(() => {
+    if (defaultPortfolio) {
+      (async () => {
+        try {
+          const res = await axios.get(`${process.env.REACT_APP_SERVER_URL}/portfolio/default/name/${defaultPortfolio}`);
+          setCurrentPortfolioName(res.data.name);
+        } catch (error) {
+          console.error(error);
+        }
+      })();
+    }
+  }, [defaultPortfolio]);
 
   const handleAddPfName = (e) => {
     setNewPortfolioName(e.target.value);
@@ -84,23 +99,44 @@ const Portfolios = ({
 
   return (
     <React.Fragment>
-      <main className="portfolio-container">
+      <main className="portfolio-main">
+        {defaultPortfolio && (
+          <div className="current-portfolio">
+            <span>Current Portfolio: </span>
+            <span>{currentPortfolioName}</span>
+          </div>
+        )}
         <Button
           btnType="button"
           btnText="Add New Portfolio"
           onClickFunc={openAddModal}
         />
-        {portfolioList.length > 0 ? (
-          <ul className="portfolio-list">
-            {portfolioList.map((portfolio) => (
-              <PortfolioItem
-                key={portfolio.portfolioId}
-                portfolio={portfolio}
-                defaultPortfolio={defaultPortfolio}
-              />
-            ))}
-          </ul>
-        ) : <div className="no-portfolio-notice">CREATE YOUR FIRST PORTFOLIO!</div>}
+        <div className="portfolios-container">
+          <header className="portfolios-container__header">Portfolios</header>
+          <div className="portfolios-table-wrapper">
+            {portfolioList.length > 0 ? (
+              <table className="portfolio-table">
+                <thead>
+                  <tr>
+                    <th className="portfolio-item__name-header">Name</th>
+                    <th className="portfolio-item__privacy-header">Privacy</th>
+                    <th className="portfolio-item__default-header">Default</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                  </tr>
+                </thead>
+                {portfolioList.map((portfolio) => (
+                  <PortfolioItem
+                    key={portfolio.portfolioId}
+                    portfolio={portfolio}
+                    defaultPortfolio={defaultPortfolio}
+                  />
+                ))}
+              </table>
+            ) : <div className="no-portfolio-notice">CREATE YOUR FIRST PORTFOLIO!</div>}
+          </div>
+        </div>
       </main>
       {isAddModalOpen &&
         <Modal closeModalFunc={closeAddModal}>
@@ -123,8 +159,8 @@ const Portfolios = ({
                 className={`${privacy === 'public' ? 'pf-privacy-btn pf-privacy-checked' : 'pf-privacy-btn'}`}
                 onClick={privacyToPublic}
               >
-                <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="globe" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" >
-                  <path fill="currentColor" d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm193.2 152h-82.5c-9-44.4-24.1-82.2-43.2-109.1 55 18.2 100.2 57.9 125.7 109.1zM336 256c0 22.9-1.6 44.2-4.3 64H164.3c-2.7-19.8-4.3-41.1-4.3-64s1.6-44.2 4.3-64h167.4c2.7 19.8 4.3 41.1 4.3 64zM248 40c26.9 0 61.4 44.1 78.1 120H169.9C186.6 84.1 221.1 40 248 40zm-67.5 10.9c-19 26.8-34.2 64.6-43.2 109.1H54.8c25.5-51.2 70.7-90.9 125.7-109.1zM32 256c0-22.3 3.4-43.8 9.7-64h90.5c-2.6 20.5-4.2 41.8-4.2 64s1.5 43.5 4.2 64H41.7c-6.3-20.2-9.7-41.7-9.7-64zm22.8 96h82.5c9 44.4 24.1 82.2 43.2 109.1-55-18.2-100.2-57.9-125.7-109.1zM248 472c-26.9 0-61.4-44.1-78.1-120h156.2c-16.7 75.9-51.2 120-78.1 120zm67.5-10.9c19-26.8 34.2-64.6 43.2-109.1h82.5c-25.5 51.2-70.7 90.9-125.7 109.1zM363.8 320c2.6-20.5 4.2-41.8 4.2-64s-1.5-43.5-4.2-64h90.5c6.3 20.2 9.7 41.7 9.7 64s-3.4 43.8-9.7 64h-90.5z" />
+                <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="lock-open" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                  <path fill="currentColor" d="M432.3 0C352.8-.2 288 64.5 288 144v48H48c-26.5 0-48 21.5-48 48v224c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V240c0-26.5-21.5-48-48-48h-64v-46.8c0-52.8 42.1-96.7 95-97.2 53.4-.6 97 42.7 97 96v56c0 13.3 10.7 24 24 24s24-10.7 24-24v-54.6C576 65.8 512 .2 432.3 0zM400 240v224H48V240h352z" />
                 </svg>
                 Public
                 </button>
@@ -133,8 +169,8 @@ const Portfolios = ({
                 className={`${privacy === 'private' ? 'pf-privacy-btn pf-privacy-checked' : 'pf-privacy-btn'}`}
                 onClick={privacyToPrivate}
               >
-                <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="lock-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" >
-                  <path fill="currentColor" d="M224 412c-15.5 0-28-12.5-28-28v-64c0-15.5 12.5-28 28-28s28 12.5 28 28v64c0 15.5-12.5 28-28 28zm224-172v224c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V240c0-26.5 21.5-48 48-48h32v-48C80 64.5 144.8-.2 224.4 0 304 .2 368 65.8 368 145.4V192h32c26.5 0 48 21.5 48 48zm-320-48h192v-48c0-52.9-43.1-96-96-96s-96 43.1-96 96v48zm272 48H48v224h352V240z" />
+                <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="lock" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                  <path fill="currentColor" d="M400 192h-32v-46.6C368 65.8 304 .2 224.4 0 144.8-.2 80 64.5 80 144v48H48c-26.5 0-48 21.5-48 48v224c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V240c0-26.5-21.5-48-48-48zm-272-48c0-52.9 43.1-96 96-96s96 43.1 96 96v48H128v-48zm272 320H48V240h352v224z" />
                 </svg>
               Private
               </button>
