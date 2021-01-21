2c8b54b0fbb6130ad19c5d0b5522a84e1703a229
jaehyeon48 <imjaehyeon48@gmail.com>
2021-01-21 12:06:50 +0900

    apply confirm modal component

--- src/components/cash/CashItem.js
+++ src/components/cash/CashItem.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import Button from '../button/Button';
-import Modal from '../modal/Modal';
+import ConfirmModal from '../modal/ConfirmModal';
 import { deleteCash } from '../../actions/cashAction';
 import { showAlert } from '../../actions/alertAction';
 
@@ -70,30 +70,11 @@ const CashItem = ({
         />
       </td>
       {isConfirmModalOpen && (
-        <Modal closeModalFunc={closeConfirmModal}>
-          <div className="delete-confirm">
-            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="exclamation-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-              <path fill="currentColor" d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zm-248 50c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z" />
-            </svg>
-            <p>
-              Do you really want to delete?
-              </p>
-            <div className="delete-confirm-actions">
-              <Button
-                btnType={'button'}
-                btnText={'Delete'}
-                btnColor={'danger'}
-                onClickFunc={handleDeleteCash}
-              />
-              <Button
-                btnType={'button'}
-                btnText={'Cancel'}
-                btnColor={'lightGray'}
-                onClickFunc={closeConfirmModal}
-              />
-            </div>
-          </div>
-        </Modal>
+        <ConfirmModal
+          confirmMsg={'Do you really want to delete the transaction record?'}
+          confirmAction={handleDeleteCash}
+          closeModalFunc={closeConfirmModal}
+        />
       )}
     </tr>
   );
