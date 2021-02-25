0fd49a041d1cd86ffacf5251591164ffa4793ff9
jaehyeon48 <imjaehyeon48@gmail.com>
2021-02-25 21:27:50 +0900

    modify bg colors and remove semi-bold font size files

---
+++ src/components/Test.js
@@ -0,0 +1,25 @@
+// import React from 'react';
+
+// const Test = () => {
+//   return (
+//     <div>
+
+//     </div>
+//   );
+// }
+
+// async function connect() {
+//   const stream = new EventSource('https://cloud-sse.iexapis.com/stable/last?token=sk_57e922fc1b44443cb08c1946504ccc3a&symbols=aapl,tsla');
+
+//   stream.addEventListener('open', (e) => {
+//     console.log('Connected')
+//   })
+
+//   stream.addEventListener('message', (e) => {
+//     console.log(JSON.parse(e.data)[0]);
+//   })
+// }
+// connect();
+
+
+// export default Test;
