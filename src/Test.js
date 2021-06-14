03e0717ffc8a241801e87c84e754d387ebeda848
jaehyeon48 <imjaehyeon48@gmail.com>
2021-06-14 21:16:45 +0900

    tradingview widget test - not using react-tradingview-widget npm library

--- src/Test.js
+++ src/Test.js
@@ -1,13 +1,27 @@
-import React, { useEffect } from 'react';
-import TradingViewWidget from 'react-tradingview-widget';
+import React, { useEffect, useRef } from 'react';
 
 const Test = () => {
 
+  useEffect(() => {
+    new window.TradingView.widget({
+      "width": 980,
+      "height": 610,
+      "symbol": "AAPL",
+      "interval": "D",
+      "timezone": "Etc/UTC",
+      "theme": "light",
+      "style": "1",
+      "locale": "en",
+      "toolbar_bg": "#f1f3f6",
+      "enable_publishing": false,
+      "allow_symbol_change": true,
+      "container_id": "tv-widget-container"
+    });
+  }, []);
+
   return (
     <div>
-      <div className="tradingview-widget-container">
-        <TradingViewWidget symbol="JPM" timezone="Asia/Seoul" />
-      </div>
+      <div id="tv-widget-container"></div>
     </div>
   );
 }
